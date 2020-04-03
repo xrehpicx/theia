3d6ae60c6a166b33082cdce84f8412c1e5709cb7
Raj <raj.fps2000@gmail.com>
2020-04-03 19:43:37 +0530

    theia deploy

--- src/motorControl.js
+++ src/motorControl.js
@@ -1,30 +1,30 @@
 const Gpio = require('pigpio').Gpio;
 module.exports = function () {
 
-    return {
-        test: () => console.log(this),
+    const worker = {
+        test: () => console.log(worker),
         init: (a, b) => {
             /* PAIR A */
-            this.ac = new Gpio(a.c, { mode: Gpio.OUTPUT });
-            this.acc = new Gpio(a.cc, { mode: Gpio.OUTPUT });
+            worker.ac = new Gpio(a.c, { mode: Gpio.OUTPUT });
+            worker.acc = new Gpio(a.cc, { mode: Gpio.OUTPUT });
             /* PAIR B */
-            this.bc = new Gpio(b.c, { mode: Gpio.OUTPUT });
-            this.bcc = new Gpio(b.cc, { mode: Gpio.OUTPUT });
+            worker.bc = new Gpio(b.c, { mode: Gpio.OUTPUT });
+            worker.bcc = new Gpio(b.cc, { mode: Gpio.OUTPUT });
             /* ENABLES FOR BOTH PAIRS */
-            this.ea = new Gpio(a.e, { mode: Gpio.OUTPUT });
-            this.eb = new Gpio(b.e, { mode: Gpio.OUTPUT });
+            worker.ea = new Gpio(a.e, { mode: Gpio.OUTPUT });
+            worker.eb = new Gpio(b.e, { mode: Gpio.OUTPUT });
 
-            this.ea.pwmWrite(0);
-            this.eb.pwmWrite(0);
-            this.yspeed = 0;
+            worker.ea.pwmWrite(0);
+            worker.eb.pwmWrite(0);
+            worker.yspeed = 0;
         },
         reset: () => {
-            this.ea.pwmWrite(0);
-            this.eb.pwmWrite(0);
-            this.ac.digitalWrite(0);
-            this.bc.digitalWrite(0);
-            this.acc.digitalWrite(0);
-            this.bcc.digitalWrite(0);
+            worker.ea.pwmWrite(0);
+            worker.eb.pwmWrite(0);
+            worker.ac.digitalWrite(0);
+            worker.bc.digitalWrite(0);
+            worker.acc.digitalWrite(0);
+            worker.bcc.digitalWrite(0);
             console.log('reset');
         },
         val: (in11, in12, in21, in22, pwm1, pwm2) => {
@@ -51,8 +51,8 @@ module.exports = function () {
             new_aSpeed = Math.floor(Math.abs(new_aSpeed));
             new_bSpeed = Math.floor(Math.abs(new_bSpeed));
 
-            this.val(acValue, accValue, bcValue, bccValue, new_aSpeed, new_bSpeed);
+            worker.val(acValue, accValue, bcValue, bccValue, new_aSpeed, new_bSpeed);
         }
     }
-
+    return worker;
 }
