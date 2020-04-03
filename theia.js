6df07d62bf3b97910aaa70aee8ce84dce9c3ac0d
Raj <raj.fps2000@gmail.com>
2020-04-03 19:30:34 +0530

    theia deploy

--- theia.js
+++ theia.js
@@ -1,3 +1,14 @@
 const wheels = require('./src/motorControl')();
+const a = {
+    c: 10,
+    cc: 9,
+    e: 27,
+}
+const b = {
+    c: 23,
+    cc: 24,
+    e: 22,
+}
+wheels.init(a, b);
 console.log(wheels.test());
-//wheels.test()
+/* wheels.test() */
