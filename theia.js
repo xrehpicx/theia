15ba9638bddfe094cab8023fbe13bd2b3c239ef2
Raj <raj.fps2000@gmail.com>
2020-04-03 20:59:21 +0530

    theia deploy

--- theia.js
+++ theia.js
@@ -1,13 +1,17 @@
 const wheels = require('./src/motorControl')();
 const socket = require('socket.io-client')('http://theiax.herokuapp.com/');
 
-socket.on('connect', function () { 
+socket.on('connect', function () {
     console.log('connected');
 });
+
 socket.on('debug', function (data) {
     console.log(data);
 });
-socket.on('disconnect', function () { });
+
+socket.on('disconnect', function () {
+    console.log('disconnected');
+});
 
 wheels.init();
 
