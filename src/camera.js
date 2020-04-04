f5de7fb8581f8004679b5f19248c59bcecd6284a
Raj <raj.fps2000@gmail.com>
2020-04-05 00:26:39 +0530

    theia deploy

--- src/camera.js
+++ src/camera.js
@@ -1,13 +1,20 @@
 const { StreamCamera, Codec } = require("pi-camera-connect");
 
-module.exports = (socket) => {
+module.exports = async (socket) => {
     const streamCamera = new StreamCamera({
         codec: Codec.MJPEG
     });
     setInterval(() => {
-        streamCamera.takeImage().then(image => {
-            socket.emit('cam', image.toString('base64'));
-        }).catch(e => console.log('camera failed'));
+        await streamCamera.startCapture();
+        const image = await streamCamera.takeImage();
+        socket.emit('cam', image.toString('base64'));
+        await streamCamera.stopCapture();
     }, 1000);
 
+
 }
+const runApp = async () => {
+
+
+};
+
