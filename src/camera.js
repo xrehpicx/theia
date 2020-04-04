b41b09948d35c87b53f0bf1cd63f2d14cc57e3cc
Raj <raj.fps2000@gmail.com>
2020-04-04 23:32:05 +0530

    theia deploy

--- src/camera.js
+++ src/camera.js
@@ -4,8 +4,10 @@ module.exports = (socket) => {
     const stillCamera = new StillCamera();
     setInterval(() => {
         stillCamera.takeImage().then(image => {
+
             socket.on('cam', image.toString('base64'));
-            /* console.log(image.toString('base64')); */
+            console.log('photo sent');
+            
         });
     }, 5000);
 
