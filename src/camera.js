ba2fb2ce04e17ff04e9f1bbcf1d4a7981d8774a7
Raj <raj.fps2000@gmail.com>
2020-04-05 10:52:57 +0530

    theia deploy

--- src/camera.js
+++ src/camera.js
@@ -4,12 +4,23 @@ module.exports = async (socket) => {
     const streamCamera = new StreamCamera({
         codec: Codec.MJPEG
     });
-    setInterval(async () => {
+    /* setInterval(async () => {
         await streamCamera.startCapture();
         const image = await streamCamera.takeImage();
         socket.emit('cam', image.toString('base64'));
         await streamCamera.stopCapture();
-    }, 1000);
+    }, 1000); */
+    const streamCamera = new StreamCamera({
+        codec: Codec.MJPEG
+    });
+
+    await streamCamera.startCapture();
+
+    const image = await streamCamera.takeImage();
+
+    // Process image...
+
+    await streamCamera.stopCapture();
 
 
 }
