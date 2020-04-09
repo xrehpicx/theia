372154050213a080340c4f130ebc4347c5ee6410
Raj <raj.fps2000@gmail.com>
2020-04-09 21:56:42 +0530

    theia deploy

--- test.js
+++ test.js
@@ -5,21 +5,22 @@ const raspberryPiCamera = require('raspberry-pi-camera-native');
 let count = 0;
 
 raspberryPiCamera.on('frame', (frameData) => {
-    const filename = 'img' + (count + '').padStart(3, '0') + '.jpg';
-    console.log('writing file: ', filename);
-    fs.writeFile(filename, frameData, (err) => {
+    //const filename = 'img' + (count + '').padStart(3, '0') + '.jpg';
+    //console.log('writing file: ', filename);
+    console.log(frameData);
+    /* fs.writeFile(filename, frameData, (err) => {
         if (err) {
             throw err;
         }
 
         count++;
-    });
+    }); */
 });
 
 raspberryPiCamera.start({
     width: 1920,
     height: 1080,
-    fps: 30,
+    fps: 0,
     quality: 50,
     encoding: 'JPEG'
 });
