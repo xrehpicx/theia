2f53fbb6300fda75d5dbe78f59cb157008c92928
Raj <raj.fps2000@gmail.com>
2020-04-09 23:50:18 +0530

    theia deploy

---
+++ camserver/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const PORT = process.env.PORT || 8000;
+
+
+http.listen(PORT, () => console.log('theia'));
