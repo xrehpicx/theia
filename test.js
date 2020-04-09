a85371e339c04a6a5cd15d7571604d4664eb8545
Raj <raj.fps2000@gmail.com>
2020-04-09 22:34:37 +0530

    theia deploy

--- test.js
+++ test.js
@@ -4,28 +4,30 @@ const socket2 = require('socket.io-client')('http://theiax.herokuapp.com/theia')
 const raspberryPiCamera = require('raspberry-pi-camera-native');
 
 let count = 0;
+socket.on('connection',()=>{
+    console.log('connected')
+    raspberryPiCamera.on('frame', (frameData) => {
+        //const filename = 'img' + (count + '').padStart(3, '0') + '.jpg';
+        //console.log('writing file: ', filename);
+        /* console.log(frameData.toString('base64')); */
+        socket.on('connection', () => console.log('connected'));
+        socket.emit('cam', frameData.toString('base64'));
+        console.log('img sent');
+        /* fs.writeFile(filename, frameData, (err) => {
+            if (err) {
+                throw err;
+            }
+    
+            count++;
+        }); */
+    });
+    raspberryPiCamera.start({
+        width: 1920 / 2,
+        height: 1080 / 2,
+        fps: 24,
+        quality: 50,
+        encoding: 'JPEG'
+    });
+})
 
-raspberryPiCamera.on('frame', (frameData) => {
-    //const filename = 'img' + (count + '').padStart(3, '0') + '.jpg';
-    //console.log('writing file: ', filename);
-    /* console.log(frameData.toString('base64')); */
-    socket.on('connection', () => console.log('connected'));
-    socket.emit('cam', frameData.toString('base64'));
-    console.log('img sent');
-    /* fs.writeFile(filename, frameData, (err) => {
-        if (err) {
-            throw err;
-        }
-
-        count++;
-    }); */
-});
-
-raspberryPiCamera.start({
-    width: 1920/2,
-    height: 1080/2,
-    fps: 24,
-    quality: 50,
-    encoding: 'JPEG'
-});
 
