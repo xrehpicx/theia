54b8c46fb9a34f9e827b0911b15b7f20808287b7
Raj <raj.fps2000@gmail.com>
2020-04-10 17:51:22 +0530

    theia deploy

--- src/camera.js
+++ src/camera.js
@@ -2,16 +2,15 @@
 const socket = require('socket.io-client')('http://theiax.herokuapp.com/cam'); */
 const raspberryPiCamera = require('raspberry-pi-camera-native');
 module.exports = {
-    init: (socket) => {
+    init: (socket, localSocket) => {
 
         socket.on('connect', () => {
             console.log('connected')
             raspberryPiCamera.on('frame', (frameData) => {
 
                 socket.on('connection', () => console.log('connected'));
-
                 socket.emit('cam', frameData);
-
+                if (localSocket) localSocket.emit('cam', frameData);
             });
             raspberryPiCamera.start({
                 width: 352,
@@ -20,6 +19,6 @@ module.exports = {
                 quality: 10,
                 encoding: 'JPEG'
             });
-        })
+        });
     }
 }
--- theia.js
+++ theia.js
@@ -1,11 +1,15 @@
 console.log('starting theia');
 const wheels = require('./src/motorControl')();
 const socket = require('socket.io-client')('http://theiax.herokuapp.com/theia');
-const camsocket = require('socket.io-client')('http://theiax.herokuapp.com/cam'); 
+const camsocket = require('socket.io-client')('http://theiax.herokuapp.com/cam');
 /* const socket = require('socket.io-client')('http://192.168.29.16:8000/theia'); */
 const camera = require('./src/camera');
-wheels.init();
+const localServer = require('./server/theia-server')();
 
+const localSocket = require('socket.io-client')('http://raspberrypi.local/theia');
+const localCamSocket = require('socket.io-client')('http://raspberrypi.local/cam');
+
+wheels.init();
 socket.on('connect', function () {
     console.log('connected to server');
     socket.emit('theia-state', '1');
@@ -28,7 +32,29 @@ socket.on('disconnect', function () {
     console.log('disconnected from server');
 });
 
-camera.init(camsocket);
+camera.init(camsocket, localCamSocket);
+
+localSocket.on('connect', function () {
+    console.log('connected to server');
+    socket.emit('theia-state', '1');
+});
+
+localSocket.on('theiay', function (y) {
+    y = Number(y);
+    /* console.log(y); */
+    wheels.goy(y)
+
+});
+
+localSocket.on('theiax', function (x) {
+    x = Number(x);
+    /* console.log(x); */
+    wheels.gox(x)
+});
+
+localSocket.on('disconnect', function () {
+    console.log('disconnected from server');
+});
 
 
 
